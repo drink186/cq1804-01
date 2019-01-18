cdfc4b6f9fbeef4fc9c9dce2c4645d9badd92108
drink186 <drink186@sina.com>
2019-01-18 17:12:41 +0800

    ddd

--- src/main/java/com/cq/entity/Hello.java
+++ src/main/java/com/cq/entity/Hello.java
@@ -3,6 +3,7 @@ package com.cq.entity;
 public class Hello {
 	public void say() {
 		System.out.println("Hello World!");
+		System.out.println("Hello World!");
 	}
 	public String hello() {
 		return "Hello World!";
