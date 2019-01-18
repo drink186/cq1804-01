06afb07c09eb5a5796e7c9fe903a763a510bf1e1
drink186 <drink186@sina.com>
2019-01-18 11:18:16 +0800

    @author drink186,@version 1.0

---
+++ src/main/java/com/cq/entity/Hello.java
@@ -0,0 +1,10 @@
+package com.cq.entity;
+
+public class Hello {
+	public void say() {
+		System.out.println("Hello World!");
+	}
+	public String hello() {
+		return "Hello World!";
+	}
+}
---
+++ src/test/java/com/cq/entity/HelloTest.java
@@ -0,0 +1,11 @@
+package com.cq.entity;
+
+import org.junit.Test;
+
+public class HelloTest {
+	Hello hello=new Hello();
+	@Test
+	public void say() {
+		hello.say();
+	}
+}
